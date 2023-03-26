37000afcb942cdf1473722e69a62b8154cc41be0
cyborggeneraal <alexvantilburg@outlook.com>
2023-03-26 09:45:22 +0200

    finished sqlalchemy tutorial

---
+++ api/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+
+from api import schemas, models
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(username = user.username, hashed_password = fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
