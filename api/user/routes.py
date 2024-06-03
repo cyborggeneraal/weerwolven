e6bc41d0c33726e26200a0b6dab2a7b379a9129e
cyborggeneraal <alexvantilburg@outlook.com>
2024-06-03 18:44:16 +0200

    fixed crud bug

--- api/user/routes.py
+++ api/user/routes.py
@@ -28,19 +28,19 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Sessio
 
 @router.post("/")
 def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)) -> schemas.User:
-    db_user = crud.crud.get_user_by_username(db, username=user.username)
+    db_user = user.crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-    return crud.crud.create_user(db=db, user=user)
+    return user.crud.create_user(db=db, user=user)
 
 @router.get("/")
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)) -> List[schemas.User]:
-    users = crud.crud.get_users(db, skip=skip, limit=limit)
+    users = user.crud.get_users(db, skip=skip, limit=limit)
     return users
 
-@router.get("/{user_id}")
-def read_user(user_id: int, db: Session = Depends(database.get_db)) -> schemas.User:
-    db_user = crud.crud.get_user(db, user_id= user_id)
+@router.get("/{username}")
+def read_user(username: str, db: Session = Depends(database.get_db)) -> schemas.User:
+    db_user = user.crud.get_user_by_username(db, username)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
